cf2d5927df9c21f7fcdff8d2f3ea64763107e92e
David Brad <davidbrad.015@gmail.com>
2016-09-28 15:32:35 -0400

    Adding a project list to the bottom

---
+++ space_game/js/System/Input.js
@@ -0,0 +1,47 @@
+function Input() {
+  var input = this;
+  var keys = [];
+  var observers = [];
+
+  this.Notify = function(key) {
+    observers.forEach(function(obv) {
+      obv(key);
+    });
+  };
+
+  this.AddObserverCallback = function( callback ) {
+    observers.push( callback );
+  };
+
+  this.ClearObservers = function() {
+    observers = [];
+  };
+
+  this.keys = {
+    13 : "ENTER",
+    38 : "UP",
+    87 : "UP",
+    40 : "DOWN",
+    83 : "DOWN",
+    37 : "LEFT",
+    65 : "LEFT",
+    39 : "RIGHT",
+    68 : "RIGHT"
+  };
+
+  $(window).keydown(function(event) {
+    var keyCode = event.which;
+    if(keys[keyCode])
+      return;
+    var VKEY = input.keys[keyCode];
+    if(VKEY && !keys[keyCode]) {
+      input.Notify(VKEY);
+    }
+    keys[keyCode] = true;
+  });
+
+  $(window).keyup(function(event) {
+      var keyCode = event.which;
+      keys[keyCode] = false;
+  });
+}
